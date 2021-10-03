65d55c40cfcd5bd9147dc3c4330b2a62a5d037d7
Teyler Halama <teyler.halama@gmail.com>
2021-10-03 16:21:13 +0100

    Added lazygit dir change function

--- functions.sh
+++ functions.sh
@@ -1,16 +1,30 @@
 function showfunc () {
     echo "----------------------------  Functions  ----------------------------"
-    echo "------------------------------  Shell  ------------------------------"
 
     echo ""
     echo "cl: code ~/zsh-config"
     echo "rosetta: Appends the -x86_64 flag before the given command"
+    echo "lg: Lazy git with dir changing upon exit"
 }
 
 function cl () {
     cd "$@" && ll
 }
 
+# Preprends Apple Rosetta x86 flag to use the cli in Rosetta mode
 function rosetta () {
     arch -x86_64 "$@"
+}
+
+# Change directory into selected repo upon exiting lazygit
+lg()
+{
+    export LAZYGIT_NEW_DIR_FILE=~/.lazygit/newdir
+
+    lazygit "$@"
+
+    if [ -f $LAZYGIT_NEW_DIR_FILE ]; then
+            cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
+            rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
+    fi
 }
