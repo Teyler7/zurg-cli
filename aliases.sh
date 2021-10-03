573f3d7ecf5ad2625aa002e8b3fcd4e925891cdd
Teyler Halama <teyler.halama@gmail.com>
2021-10-03 16:54:13 +0100

    Added npm ViteJS alias

--- aliases.sh
+++ aliases.sh
@@ -3,8 +3,13 @@ function showalias () {
     echo ""
     echo "modzsh: code ~/zsh-config"
     echo "modzurg: code ~/zurg-cli"
+    echo "-------------------------------  NPM  -------------------------------"
+    echo "vite: Scaffold new project via ViteJS (https://github.com/vitejs/vite)"
 }
 
 # Shell
 alias modzsh="code ~/zsh-config"
 alias modzurg="code ~/zurg-cli"
+
+# NPM
+alias vite="npm init vite"
