1a5bdf4ce5d1197a67f891abcd28af8589f260f0
Teyler Halama <teyler@hey.com>
2023-01-09 14:36:47 -0700

    Updated functions, ssh keychain ref, and ipqos fix

--- functions.sh
+++ functions.sh
@@ -10,10 +10,6 @@ function showfunc () {
     echo "fbr: Fuzzy find and checkout branch"
 }
 
-function cl () {
-    cd "$@" && ll
-}
-
 # Change directory into selected repo upon exiting lazygit
 lg()
 {
@@ -36,13 +32,7 @@ fd() {
 }
 
 fch() {
-    git rev-parse HEAD > /dev/null 2>&1 || return
-
-    git branch --color=always --all --sort=-committerdate |
-        grep -v HEAD |
-        fzf --height 50% --ansi --no-multi --preview-window right:65% \
-            --preview 'git log -n 50 --color=always --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed "s/.* //" <<< {})' |
-        sed "s/.* //"
+    git checkout $(git for-each-ref refs/heads/ --format='%(refname:short)' | fzf)
 }
 
 fshow() {
@@ -54,12 +44,4 @@ fshow() {
                 xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
                 {}
   FZF-EOF"
-}
-
-fbr() {
-  local branches branch
-  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
-  branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
